54b62bbadef9cccd8cd91d413e0b04ed6c5bce14
Miromind <45048681+miromind@users.noreply.github.com>
2025-10-26 00:52:03 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const frame = {
   version: 'next',
   imageUrl: `${APP_URL}/images/treasure.png`,
   button: {
-    title: 'Launch Template',
+    title: 'Join',
     action: {
       type: 'launch_frame',
       name: 'Treasure Hunt Farcaster MiniApp by PiracyData',
