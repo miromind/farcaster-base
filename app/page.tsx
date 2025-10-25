6c0d09507ac01f520c9976091d5583bb75dceefe
Miromind <45048681+miromind@users.noreply.github.com>
2025-10-26 00:31:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const frame = {
     title: 'Launch Template',
     action: {
       type: 'launch_frame',
-      name: 'Trankil Farcaster MiniApp',
+      name: 'Treasure Hunt Farcaster MiniApp by PiracyData',
       url: APP_URL,
       splashImageUrl: `${APP_URL}/images/splash.png`,
       splashBackgroundColor: '#f7f7f7',
@@ -19,10 +19,10 @@ const frame = {
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: 'Trankil Farcaster MiniApp',
+    title: 'Treasure Hunt Farcaster MiniApp by PiracyData',
     openGraph: {
-      title: 'Trankil Farcaster MiniApp',
-      description: 'A template for building mini-apps on Farcaster and Base',
+      title: 'Treasure Hunt Farcaster MiniApp by PiracyData',
+      description: 'Treasure Hunt Farcaster MiniApp by PiracyData',
     },
     other: {
       'fc:frame': JSON.stringify(frame),
